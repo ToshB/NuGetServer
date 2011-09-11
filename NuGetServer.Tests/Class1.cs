d19a8f975831275ad0dbf50aceb9ef0aec2e7529
Torstein Bjørnstad <torstein.bjornstad@gyldendal.no>
2011-09-11 18:29:01 +0200

    added sample tests

---
+++ NuGetServer.Tests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NuGetServer.Tests
+{   
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void MethodName_WithCondition_ExpectedOutcome()
+        {
+            Assert.IsTrue(true);
+        }
+
+
+        [Test]
+        public void Method2Name_WithCondition_ExpectedOutcome()
+        {
+            Assert.IsTrue(false, "Something failed");
+        }
+    }
+}
