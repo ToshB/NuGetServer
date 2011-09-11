0d07c5ce0cecde567efa83913044f2cba6e8cc6a
Torstein Bjørnstad <torstein.bjornstad@gyldendal.no>
2011-09-11 17:38:14 +0200

    fixed charset bug

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-﻿$oldDir = $pwd
+$oldDir = $pwd
 cd (Split-Path -Parent $MyInvocation.MyCommand.Definition)
 Import-Module (Resolve-Path .\packages\psake*\tools\psake.psm1)
 Invoke-psake .\default.ps1 $args
