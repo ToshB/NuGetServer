0fd9a4dd86a22a43e287f8992b688c58eaa7c065
Torstein Bjørnstad <torstein.bjornstad@gyldendal.no>
2011-09-11 17:35:34 +0200

    tweaking

--- build.ps1
+++ build.ps1
@@ -1,3 +1,6 @@
-﻿Import-Module (Resolve-Path .\packages\psake*\tools\psake.psm1)
-Invoke-psake .\Build\default.ps1 $args
+﻿$oldDir = $pwd
+cd (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+Import-Module (Resolve-Path .\packages\psake*\tools\psake.psm1)
+Invoke-psake .\default.ps1 $args
 Remove-Module psake
+cd $oldDir
---
+++ default.ps1
@@ -0,0 +1,7 @@
+﻿# psake .\default.ps1 build -parameters @{Whatif=1}
+
+Import-Module .\Build\common.ps1
+
+task SolutionSpecific -depends Init {
+	Write-Host "I could be replaced with a task doing something solution specific"
+}
